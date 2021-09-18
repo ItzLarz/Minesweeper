0aa275545b5e4c2df740021460ea35e7657687b2
ItzLarz <l.y.kruitwagen@gmail.com>
2021-09-18 16:29:38 +0200

    fixed formatting

--- MineSweeper.js
+++ MineSweeper.js
@@ -2,51 +2,39 @@ var bombs = 20;
 let squares = [];
 function setup() 
 {
-    createCanvas(375, 250);
-    background(220);
-    drawField();
-    selectBombs();
+  createCanvas(375, 250);
+  background(220);
+  drawField();
+  selectBombs();
+}
+
+function draw() { }
+
+function drawField() 
+{
+  for (i = 0; i <= 370; i += 25) {
+    for (j = 0; j <= 245; j += 25) {
+      square(i, j, 20);
+      append(squares, "x" + i + "y" + j);
+    }
   }
-  
-function draw() 
-{
-    
-  
 }
 
-function drawField()
+function selectBombs() 
 {
-  for (i = 0; i<=370; i+=25)
-    {
-      for (j = 0; j<=245; j+=25)
-        {
-          square(i,j,20);
-          append(squares, "x" + i + "y" + j);
-        }
+  var bombCounter = 0;
+  while (bombCounter < bombs) {
+    var pickedSquare = random(squares);
+    if (pickedSquare.safety != "bomb") {
+      pickedSquare.safety = "bomb";
+      bombCounter++;
+      console.log(pickedSquare);
     }
-}
 
-
-
-
-
-function selectBombs()
-{
-    var bombCounter = 0;
-    while(bombCounter < bombs)
-    {
-        var pickedSquare = random(squares);
-        if(pickedSquare.safety != "bomb")
-        {
-            pickedSquare.safety = "bomb";
-            bombCounter++;
-            console.log(pickedSquare);
-        }
-      
-        else{
-          console.log("ghello");
-        }
-      
-        console.log(bombCounter);
+    else {
+      console.log("ghello");
     }
+
+    console.log(bombCounter);
+  }
 }
