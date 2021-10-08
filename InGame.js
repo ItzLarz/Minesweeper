c925fa598b2128a3d3e4e07595d781e782aa0541
ItzLarz <l.y.kruitwagen@gmail.com>
2021-10-08 08:46:59 +0200

    fix

--- InGame.js
+++ InGame.js
@@ -73,49 +73,51 @@ function uncover(square)
   {
     for (j = 0; j < rows; j++)
     {
-  if(square.covered == false) {
-    switch (square.value)
-    {
-      case 0:
-        image(square0, square.rawx, square.rawy);
-          uncoverSurrounding(i, j, square);
-        break;
-      
-      case 1:
-        image(square1, square.rawx, square.rawy);
-        break;
-        
-      case 2:
-        image(square2, square.rawx, square.rawy);
-        break;
+      if(square.covered == false) 
+      {
+        switch (square.value)
+        {
+          case 0:
+            image(square0, square.rawx, square.rawy);
+              uncoverSurrounding(i, j, square);
+            break;
+          
+          case 1:
+            image(square1, square.rawx, square.rawy);
+            break;
+            
+          case 2:
+            image(square2, square.rawx, square.rawy);
+            break;
 
-      case 3:
-        image(square3, square.rawx, square.rawy);
-        break;
+          case 3:
+            image(square3, square.rawx, square.rawy);
+            break;
 
-      case 4:
-        image(square4, square.rawx, square.rawy);
-        break;
+          case 4:
+            image(square4, square.rawx, square.rawy);
+            break;
 
-      case 5:
-        image(square5, square.rawx, square.rawy);
-        break;
-      
-      case 6:
-        image(square6, square.rawx, square.rawy);
-        break;
-        
-      case 7:
-        image(square7, square.rawx, square.rawy);
-        break;
+          case 5:
+            image(square5, square.rawx, square.rawy);
+            break;
+          
+          case 6:
+            image(square6, square.rawx, square.rawy);
+            break;
+            
+          case 7:
+            image(square7, square.rawx, square.rawy);
+            break;
 
-      case 8:
-        image(square8, square.rawx, square.rawy);
-        break;
+          case 8:
+            image(square8, square.rawx, square.rawy);
+            break;
+        }
+      }
     }
   }
-    }
-  }
+}
 
 function uncoverSurrounding(i, j, square)
 {
@@ -207,5 +209,4 @@ function uncoverSurrounding(i, j, square)
     }
     uncover(square);
   }
-}
 }
