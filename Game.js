3dba9539d4fadf54167d967cda76729cec0cc7d3
ItzLarz <l.y.kruitwagen@gmail.com>
2021-10-27 12:31:30 +0200

    prettier

--- Game.js
+++ Game.js
@@ -1,87 +1,67 @@
-function mouseReleased()
-{
+function mouseReleased() {
   let x = mouseX;
   let y = mouseY;
   let button;
-  
-  if (mouseButton === LEFT)
-  {
+
+  if (mouseButton === LEFT) {
     button = "left";
   }
 
-  else if (mouseButton === RIGHT)
-  {
+  else if (mouseButton === RIGHT) {
     button = "right";
   }
 
-  if (!gameOver)
-  {
+  if (!gameOver) {
     let col = Math.floor((x - edgeSize) / boxSize);
     let row = Math.floor((y - edgeSize - topBorderSize) / boxSize);
     let square = findSquare(col, row);
-    
-    try
-    {
-      if (button === "left" && square.covered === true && square.marked === false)
-      {
+
+    try {
+      if (button === "left" && square.covered === true && square.marked === false) {
         square.covered = false;
 
-        if (square.bomb === true)
-        {
+        if (square.bomb === true) {
           gameOver = true;
           defeat = true;
           image(squareRedMine, square.rawx, square.rawy, boxSize, boxSize);
-          for (i = 0; i < columns; i++)
-          {
-            for (j = 0; j < rows; j++)
-            {
-              if (squares[i][j].bomb == true && squares[i][j].covered == true && squares[i][j].marked == false)
-              {
+          for (i = 0; i < columns; i++) {
+            for (j = 0; j < rows; j++) {
+              if (squares[i][j].bomb == true && squares[i][j].covered == true && squares[i][j].marked == false) {
                 uncover(squares[i][j]);
               }
 
-              else if (squares[i][j].marked == true && squares[i][j].bomb == false)
-              {
+              else if (squares[i][j].marked == true && squares[i][j].bomb == false) {
                 image(squareNotMine, squares[i][j].rawx, squares[i][j].rawy, boxSize, boxSize);
               }
             }
           }
-          
+
           gameOverScreen();
         }
 
-        else if (square.bomb === false)
-        {
+        else if (square.bomb === false) {
           uncover(square);
           let gameOverCheck = true;
-          for (i = 0; i < columns; i++)
-          {
-            for (j = 0; j < rows; j++)
-            {
-              if (squares[i][j].covered == true && squares[i][j].bomb == false)
-              {
+          for (i = 0; i < columns; i++) {
+            for (j = 0; j < rows; j++) {
+              if (squares[i][j].covered == true && squares[i][j].bomb == false) {
                 gameOverCheck = false;
                 break;
               }
             }
-            
-            if (!gameOverCheck)
-            {
+
+            if (!gameOverCheck) {
               break;
             }
           }
 
-          if (gameOverCheck)
-          {
+          if (gameOverCheck) {
             gameOver = true;
             win = true;
 
-            for (i = 0; i < columns; i++)
-            {
-              for (j = 0; j < rows; j++)
-              {
-                if (squares[i][j].bomb == true && squares[i][j].covered == true && squares[i][j].marked == false)
-                {
+            for (i = 0; i < columns; i++) {
+              for (j = 0; j < rows; j++) {
+                if (squares[i][j].bomb == true && squares[i][j].covered == true && squares[i][j].marked == false) {
                   squares[i][j].marked = false;
                   squares[i][j].covered = false;
                   image(squareFlag, squares[i][j].rawx, squares[i][j].rawy, boxSize, boxSize);
@@ -89,7 +69,7 @@ function mouseReleased()
                 }
               }
             }
-            
+
             textAlign(CENTER);
             textSize(columns);
             strokeWeight(0);
@@ -103,24 +83,21 @@ function mouseReleased()
         }
       }
 
-      else if (button === "right" && square.covered === true)
-      {
-        
+      else if (button === "right" && square.covered === true) {
 
-        if (square.marked === false)
-        {
+
+        if (square.marked === false) {
           square.marked = true;
           image(squareFlag, square.rawx, square.rawy, boxSize, boxSize);
           bombCount--;
         }
 
-        else if (square.marked === true)
-        {
+        else if (square.marked === true) {
           square.marked = false;
           image(squareBlank, square.rawx, square.rawy, boxSize, boxSize);
           bombCount++;
         }
-        
+
         textAlign(CENTER);
         textSize(columns);
         strokeWeight(0);
@@ -130,44 +107,37 @@ function mouseReleased()
         text(bombCount, edgeSize + (boxSize * columns) / 6, topBorderSize / 1.33);
       }
     }
-    
-    catch (TypeError){}
+
+    catch (TypeError) { }
   }
 
-  if (button === "left" && x > edgeSize + (boxSize * columns / 2) - (columns * 3.33) && x < edgeSize + (boxSize * columns / 2) + (columns * 3.33) && y > topBorderSize / 20 && y < topBorderSize - (topBorderSize / 20))
-  {
+  if (button === "left" && x > edgeSize + (boxSize * columns / 2) - (columns * 3.33) && x < edgeSize + (boxSize * columns / 2) + (columns * 3.33) && y > topBorderSize / 20 && y < topBorderSize - (topBorderSize / 20)) {
     reset();
   }
 }
 
-function findSquare(col, row)
-{
-  for (i = 0; i < columns; i++)
-  {
-    for (j = 0; j < rows; j++)
-    {
-      if (squares[i][j].col === col && squares[i][j].row === row)
-      {
+function findSquare(col, row) {
+  for (i = 0; i < columns; i++) {
+    for (j = 0; j < rows; j++) {
+      if (squares[i][j].col === col && squares[i][j].row === row) {
         return squares[i][j];
       }
     }
   }
 }
 
-function uncover(square) 
-{
+function uncover(square) {
   square.covered == false;
 
-  switch (square.value)
-  {
+  switch (square.value) {
     case 0:
       whiteSquare(square);
       break;
-    
+
     case 1:
       image(square1, square.rawx, square.rawy, boxSize, boxSize);
       break;
-      
+
     case 2:
       image(square2, square.rawx, square.rawy, boxSize, boxSize);
       break;
@@ -183,11 +153,11 @@ function uncover(square)
     case 5:
       image(square5, square.rawx, square.rawy, boxSize, boxSize);
       break;
-    
+
     case 6:
       image(square6, square.rawx, square.rawy, boxSize, boxSize);
       break;
-      
+
     case 7:
       image(square7, square.rawx, square.rawy, boxSize, boxSize);
       break;
@@ -200,29 +170,24 @@ function uncover(square)
       image(squareMine, square.rawx, square.rawy, boxSize, boxSize);
       break;
   }
-} 
+}
 
-function whiteSquare(square)
-{
+function whiteSquare(square) {
   uncoverList = [];
   uncoverList.push(square);
   let length = 1;
   let newLength = 0;
-  while(newLength != length)
-  {
+  while (newLength != length) {
     length = uncoverList.length;
-    for (i = 0; i < length; i++)
-    {
+    for (i = 0; i < length; i++) {
       uncoverSurrounding(uncoverList[i]);
     }
     uncoverList = [...new Set(uncoverList)];
     newLength = uncoverList.length;
   }
-  
-  for(i = 0; i < uncoverList.length; i++)
-  {
-    if (uncoverList[i].marked)
-    {
+
+  for (i = 0; i < uncoverList.length; i++) {
+    if (uncoverList[i].marked) {
       uncoverList[i].marked = false;
       bombCount++
 
@@ -236,17 +201,16 @@ function whiteSquare(square)
     }
 
     uncoverList[i].covered = false;
-     
-    switch (uncoverList[i].value)
-    {
+
+    switch (uncoverList[i].value) {
       case 0:
         image(square0, uncoverList[i].rawx, uncoverList[i].rawy, boxSize, boxSize);
         break;
-      
+
       case 1:
         image(square1, uncoverList[i].rawx, uncoverList[i].rawy, boxSize, boxSize);
         break;
-        
+
       case 2:
         image(square2, uncoverList[i].rawx, uncoverList[i].rawy, boxSize, boxSize);
         break;
@@ -262,11 +226,11 @@ function whiteSquare(square)
       case 5:
         image(square5, uncoverList[i].rawx, uncoverList[i].rawy, boxSize, boxSize);
         break;
-      
+
       case 6:
         image(square6, uncoverList[i].rawx, uncoverList[i].rawy, boxSize, boxSize);
         break;
-        
+
       case 7:
         image(square7, uncoverList[i].rawx, uncoverList[i].rawy, boxSize, boxSize);
         break;
@@ -278,124 +242,110 @@ function whiteSquare(square)
   }
 }
 
-function uncoverSurrounding(square)
-{
-  if (square.value == 0)
-  {
+function uncoverSurrounding(square) {
+  if (square.value == 0) {
     // Upper left corner
-    if (square.col === 0 && square.row === 0)
-    {
+    if (square.col === 0 && square.row === 0) {
       uncoverList.push(squares[0][1]);
       uncoverList.push(squares[1][0]);
       uncoverList.push(squares[1][1]);
-      
+
     }
-    
+
     // Upper right corner
-    else if (square.col === columns - 1 && square.row === 0)
-    {
-      uncoverList.push(squares[columns-2][0]);
-      uncoverList.push(squares[columns-1][1]);
-      uncoverList.push(squares[columns-2][1]);
+    else if (square.col === columns - 1 && square.row === 0) {
+      uncoverList.push(squares[columns - 2][0]);
+      uncoverList.push(squares[columns - 1][1]);
+      uncoverList.push(squares[columns - 2][1]);
     }
 
     // Bottom left corner
-    else if (square.col === 0 && square.row === rows - 1)
-    {
-      uncoverList.push(squares[0][rows-2]);
-      uncoverList.push(squares[1][rows-1]);
-      uncoverList.push(squares[1][rows-2]);
+    else if (square.col === 0 && square.row === rows - 1) {
+      uncoverList.push(squares[0][rows - 2]);
+      uncoverList.push(squares[1][rows - 1]);
+      uncoverList.push(squares[1][rows - 2]);
     }
 
     // Bottom right corner
-    else if (square.col === columns - 1 && square.row === rows - 1)
-    {
-      uncoverList.push(squares[columns-1][rows-2]);
-      uncoverList.push(squares[columns-2][rows-1]);
-      uncoverList.push(squares[columns-2][rows-2]);
+    else if (square.col === columns - 1 && square.row === rows - 1) {
+      uncoverList.push(squares[columns - 1][rows - 2]);
+      uncoverList.push(squares[columns - 2][rows - 1]);
+      uncoverList.push(squares[columns - 2][rows - 2]);
     }
 
     // Left wall
-    else if (square.col === 0 && square.row != 0)
-    {
-      uncoverList.push(squares[square.col][square.row-1]);
-      uncoverList.push(squares[square.col+1][square.row-1]);
-      uncoverList.push(squares[square.col+1][square.row]);
-      uncoverList.push(squares[square.col+1][square.row+1]);
-      uncoverList.push(squares[square.col][square.row+1]);
+    else if (square.col === 0 && square.row != 0) {
+      uncoverList.push(squares[square.col][square.row - 1]);
+      uncoverList.push(squares[square.col + 1][square.row - 1]);
+      uncoverList.push(squares[square.col + 1][square.row]);
+      uncoverList.push(squares[square.col + 1][square.row + 1]);
+      uncoverList.push(squares[square.col][square.row + 1]);
     }
-    
+
     // Right wall
-    else if (square.col === columns - 1 && square.row != 0)
-    {
-      uncoverList.push(squares[square.col][square.row-1]);
-      uncoverList.push(squares[square.col-1][square.row-1]);
-      uncoverList.push(squares[square.col-1][square.row]);
-      uncoverList.push(squares[square.col-1][square.row+1]);
-      uncoverList.push(squares[square.col][square.row+1]);
+    else if (square.col === columns - 1 && square.row != 0) {
+      uncoverList.push(squares[square.col][square.row - 1]);
+      uncoverList.push(squares[square.col - 1][square.row - 1]);
+      uncoverList.push(squares[square.col - 1][square.row]);
+      uncoverList.push(squares[square.col - 1][square.row + 1]);
+      uncoverList.push(squares[square.col][square.row + 1]);
     }
 
     // Upper wall
-    else if (square.col != 0 && square.row === 0)
-    {
-      uncoverList.push(squares[square.col-1][square.row]);
-      uncoverList.push(squares[square.col-1][square.row+1]);
-      uncoverList.push(squares[square.col][square.row+1]);
-      uncoverList.push(squares[square.col+1][square.row+1]);
-      uncoverList.push(squares[square.col+1][square.row]);
+    else if (square.col != 0 && square.row === 0) {
+      uncoverList.push(squares[square.col - 1][square.row]);
+      uncoverList.push(squares[square.col - 1][square.row + 1]);
+      uncoverList.push(squares[square.col][square.row + 1]);
+      uncoverList.push(squares[square.col + 1][square.row + 1]);
+      uncoverList.push(squares[square.col + 1][square.row]);
     }
 
     // Bottom wall
-    else if (square.col != 0 && square.row === rows - 1)
-    {
-      uncoverList.push(squares[square.col-1][square.row]);
-      uncoverList.push(squares[square.col-1][square.row-1]);
-      uncoverList.push(squares[square.col][square.row-1]);
-      uncoverList.push(squares[square.col+1][square.row-1]);
-      uncoverList.push(squares[square.col+1][square.row]);
+    else if (square.col != 0 && square.row === rows - 1) {
+      uncoverList.push(squares[square.col - 1][square.row]);
+      uncoverList.push(squares[square.col - 1][square.row - 1]);
+      uncoverList.push(squares[square.col][square.row - 1]);
+      uncoverList.push(squares[square.col + 1][square.row - 1]);
+      uncoverList.push(squares[square.col + 1][square.row]);
     }
 
     // Rest of the squares
-    else if (square.col < columns && square.row < rows)
-    {
-      uncoverList.push(squares[square.col-1][square.row]);
-      uncoverList.push(squares[square.col-1][square.row-1]);
-      uncoverList.push(squares[square.col][square.row-1]);
-      uncoverList.push(squares[square.col+1][square.row-1]);
+    else if (square.col < columns && square.row < rows) {
+      uncoverList.push(squares[square.col - 1][square.row]);
+      uncoverList.push(squares[square.col - 1][square.row - 1]);
+      uncoverList.push(squares[square.col][square.row - 1]);
+      uncoverList.push(squares[square.col + 1][square.row - 1]);
 
-      uncoverList.push(squares[square.col+1][square.row]);
-      uncoverList.push(squares[square.col+1][square.row+1]);
-      uncoverList.push(squares[square.col][square.row+1]);
-      uncoverList.push(squares[square.col-1][square.row+1]);
+      uncoverList.push(squares[square.col + 1][square.row]);
+      uncoverList.push(squares[square.col + 1][square.row + 1]);
+      uncoverList.push(squares[square.col][square.row + 1]);
+      uncoverList.push(squares[square.col - 1][square.row + 1]);
     }
   }
 }
 
-function gameOverScreen()
-{
-  if (defeat)
-  {
+function gameOverScreen() {
+  if (defeat) {
     fill(200);
     stroke(170);
     strokeWeight(5);
     rectMode(CENTER);
     rect(edgeSize + (boxSize * columns) / 2, topBorderSize / 2, columns * 6.66, topBorderSize - topBorderSize / 10, columns);
-    
+
     textAlign(CENTER);
     textSize(columns * 1.3);
     fill(0);
     strokeWeight(0);
     text("œf, defeat", edgeSize + (boxSize * columns) / 2, topBorderSize / 1.6)
   }
-  
-  if (win)
-  {
+
+  if (win) {
     fill(200);
     stroke(170);
     strokeWeight(5);
     rectMode(CENTER);
     rect(edgeSize + (boxSize * columns) / 2, topBorderSize / 2, columns * 6.66, topBorderSize - topBorderSize / 10, columns);
-    
+
     textAlign(CENTER);
     textSize(columns * 1.3);
     fill(0);
@@ -404,8 +354,7 @@ function gameOverScreen()
   }
 }
 
-function reset()
-{
+function reset() {
   squares = [];
   bombList = [];
   uncoverList = [];
--- Initialize.js
+++ Initialize.js
@@ -1,5 +1,4 @@
-document.oncontextmenu = function() 
-{
+document.oncontextmenu = function () {
   return false;
 };
 
@@ -16,7 +15,7 @@ var bombList = [];
 var uncoverList = [];
 var gameOver = false;
 var win = false;
-var defeat = false; 
+var defeat = false;
 var bombCount = bombs;
 var interval = 0;
 var minutes = 0;
@@ -38,8 +37,7 @@ let squareMine;
 let squareNotMine;
 let squareRedMine;
 
-function preload() 
-{
+function preload() {
   square0 = loadImage("img/0.png");
   square1 = loadImage("img/1.png");
   square2 = loadImage("img/2.png");
@@ -56,8 +54,7 @@ function preload()
   squareRedMine = loadImage("img/redmine.png");
 }
 
-function setup() 
-{
+function setup() {
   createCanvas(2 * edgeSize + (boxSize * columns), topBorderSize + 2 * edgeSize + (boxSize * rows));
   background(100);
   drawField();
@@ -67,12 +64,9 @@ function setup()
   console.log(squares);
 }
 
-function draw() 
-{ 
-  if (!gameOver)
-  {
-    if (millis() - interval >= 1000)
-    {
+function draw() {
+  if (!gameOver) {
+    if (millis() - interval >= 1000) {
       interval = millis();
       seconds += 1;
       textAlign(CENTER);
@@ -82,34 +76,28 @@ function draw()
       rect(edgeSize + (boxSize * columns) / 1.2, topBorderSize / 1.55, columns * 6, topBorderSize - topBorderSize / 1.4);
       fill(0);
 
-      if (seconds >= 60)
-      {
+      if (seconds >= 60) {
         minutes += 1;
         seconds = 0;
       }
 
-      if (seconds >= 10)
-      {
+      if (seconds >= 10) {
         spacer = "";
       }
 
-      else 
-      {
+      else {
         spacer = 0;
       }
 
       text(minutes + ":" + spacer + seconds, edgeSize + (boxSize * columns) / 1.2, topBorderSize / 1.33);
-    } 
+    }
   }
 }
 
-function drawField() 
-{
-  for (i = 0; i < columns; i++) 
-  {
+function drawField() {
+  for (i = 0; i < columns; i++) {
     let tempList = [];
-    for (j = 0; j < rows; j++) 
-    {
+    for (j = 0; j < rows; j++) {
       let object = new Object();
       object.col = i;
       object.row = j;
@@ -126,14 +114,11 @@ function drawField()
   }
 }
 
-function selectBombs() 
-{
-  while (bombList.length < bombs) 
-  {
+function selectBombs() {
+  while (bombList.length < bombs) {
     let pickedLine = random(squares);
     let pickedSquare = random(pickedLine);
-    if (pickedSquare.bomb != true) 
-    {
+    if (pickedSquare.bomb != true) {
       bombList.push(pickedSquare);
       pickedSquare.bomb = true;
       pickedSquare.value = null;
@@ -141,110 +126,96 @@ function selectBombs()
   }
 }
 
-function calcValue()
-{
-  for (i = 0; i < columns; i++) 
-  {
-    for (j = 0; j < rows; j++) 
-    {
+function calcValue() {
+  for (i = 0; i < columns; i++) {
+    for (j = 0; j < rows; j++) {
       let countBombs = 0;
-      if (squares[i][j].bomb === true)
-      {
+      if (squares[i][j].bomb === true) {
         continue;
       }
 
       // Upper left corner
-      if (i === 0 && j === 0)
-      {
-        if (squares[0][1].bomb === true){countBombs++;}
-        if (squares[1][0].bomb === true){countBombs++;}
-        if (squares[1][1].bomb === true){countBombs++;}
+      if (i === 0 && j === 0) {
+        if (squares[0][1].bomb === true) { countBombs++; }
+        if (squares[1][0].bomb === true) { countBombs++; }
+        if (squares[1][1].bomb === true) { countBombs++; }
       }
-      
+
       // Upper right corner
-      else if (i === columns - 1 && j === 0)
-      {
-        if (squares[columns-2][0].bomb === true){countBombs++;}
-        if (squares[columns-1][1].bomb === true){countBombs++;}
-        if (squares[columns-2][1].bomb === true){countBombs++;}
+      else if (i === columns - 1 && j === 0) {
+        if (squares[columns - 2][0].bomb === true) { countBombs++; }
+        if (squares[columns - 1][1].bomb === true) { countBombs++; }
+        if (squares[columns - 2][1].bomb === true) { countBombs++; }
       }
 
       // Bottom left corner
-      else if (i === 0 && j === rows - 1)
-      {
-        if (squares[0][rows-2].bomb === true){countBombs++;}
-        if (squares[1][rows-1].bomb === true){countBombs++;}
-        if (squares[1][rows-2].bomb === true){countBombs++;}
+      else if (i === 0 && j === rows - 1) {
+        if (squares[0][rows - 2].bomb === true) { countBombs++; }
+        if (squares[1][rows - 1].bomb === true) { countBombs++; }
+        if (squares[1][rows - 2].bomb === true) { countBombs++; }
       }
 
       // Bottom right corner
-      else if (i === columns - 1 && j === rows - 1)
-      {
-        if (squares[columns-1][rows-2].bomb === true){countBombs++;}
-        if (squares[columns-2][rows-1].bomb === true){countBombs++;}
-        if (squares[columns-2][rows-2].bomb === true){countBombs++;}
+      else if (i === columns - 1 && j === rows - 1) {
+        if (squares[columns - 1][rows - 2].bomb === true) { countBombs++; }
+        if (squares[columns - 2][rows - 1].bomb === true) { countBombs++; }
+        if (squares[columns - 2][rows - 2].bomb === true) { countBombs++; }
       }
 
       // Left wall
-      else if (i === 0 && j != 0)
-      {
-        if (squares[i][j-1].bomb === true){countBombs++;}
-        if (squares[i+1][j-1].bomb === true){countBombs++;}
-        if (squares[i+1][j].bomb === true){countBombs++;}
-        if (squares[i+1][j+1].bomb === true){countBombs++;}
-        if (squares[i][j+1].bomb === true){countBombs++;}
+      else if (i === 0 && j != 0) {
+        if (squares[i][j - 1].bomb === true) { countBombs++; }
+        if (squares[i + 1][j - 1].bomb === true) { countBombs++; }
+        if (squares[i + 1][j].bomb === true) { countBombs++; }
+        if (squares[i + 1][j + 1].bomb === true) { countBombs++; }
+        if (squares[i][j + 1].bomb === true) { countBombs++; }
       }
-      
+
       // Right wall
-      else if (i === columns - 1 && j != 0)
-      {
-        if (squares[i][j-1].bomb === true){countBombs++;}
-        if (squares[i-1][j-1].bomb === true){countBombs++;}
-        if (squares[i-1][j].bomb === true){countBombs++;}
-        if (squares[i-1][j+1].bomb === true){countBombs++;}
-        if (squares[i][j+1].bomb === true){countBombs++;}
+      else if (i === columns - 1 && j != 0) {
+        if (squares[i][j - 1].bomb === true) { countBombs++; }
+        if (squares[i - 1][j - 1].bomb === true) { countBombs++; }
+        if (squares[i - 1][j].bomb === true) { countBombs++; }
+        if (squares[i - 1][j + 1].bomb === true) { countBombs++; }
+        if (squares[i][j + 1].bomb === true) { countBombs++; }
       }
 
       // Upper wall
-      else if (i != 0 && j === 0)
-      {
-        if (squares[i-1][j].bomb === true){countBombs++;}
-        if (squares[i-1][j+1].bomb === true){countBombs++;}
-        if (squares[i][j+1].bomb === true){countBombs++;}
-        if (squares[i+1][j+1].bomb === true){countBombs++;}
-        if (squares[i+1][j].bomb === true){countBombs++;}
+      else if (i != 0 && j === 0) {
+        if (squares[i - 1][j].bomb === true) { countBombs++; }
+        if (squares[i - 1][j + 1].bomb === true) { countBombs++; }
+        if (squares[i][j + 1].bomb === true) { countBombs++; }
+        if (squares[i + 1][j + 1].bomb === true) { countBombs++; }
+        if (squares[i + 1][j].bomb === true) { countBombs++; }
       }
 
       // Bottom wall
-      else if (i != 0 && j === rows - 1)
-      {
-        if (squares[i-1][j].bomb === true){countBombs++;}
-        if (squares[i-1][j-1].bomb === true){countBombs++;}
-        if (squares[i][j-1].bomb === true){countBombs++;}
-        if (squares[i+1][j-1].bomb === true){countBombs++;}
-        if (squares[i+1][j].bomb === true){countBombs++;}
+      else if (i != 0 && j === rows - 1) {
+        if (squares[i - 1][j].bomb === true) { countBombs++; }
+        if (squares[i - 1][j - 1].bomb === true) { countBombs++; }
+        if (squares[i][j - 1].bomb === true) { countBombs++; }
+        if (squares[i + 1][j - 1].bomb === true) { countBombs++; }
+        if (squares[i + 1][j].bomb === true) { countBombs++; }
       }
 
       // Rest of the squares
-      else if (i < columns && j < rows)
-      {
-        if (squares[i-1][j].bomb === true){countBombs++;}
-        if (squares[i-1][j-1].bomb === true){countBombs++;}
-        if (squares[i][j-1].bomb === true){countBombs++;}
-        if (squares[i+1][j-1].bomb === true){countBombs++;}
+      else if (i < columns && j < rows) {
+        if (squares[i - 1][j].bomb === true) { countBombs++; }
+        if (squares[i - 1][j - 1].bomb === true) { countBombs++; }
+        if (squares[i][j - 1].bomb === true) { countBombs++; }
+        if (squares[i + 1][j - 1].bomb === true) { countBombs++; }
 
-        if (squares[i+1][j].bomb === true){countBombs++;}
-        if (squares[i+1][j+1].bomb === true){countBombs++;}
-        if (squares[i][j+1].bomb === true){countBombs++;}
-        if (squares[i-1][j+1].bomb === true){countBombs++;}
+        if (squares[i + 1][j].bomb === true) { countBombs++; }
+        if (squares[i + 1][j + 1].bomb === true) { countBombs++; }
+        if (squares[i][j + 1].bomb === true) { countBombs++; }
+        if (squares[i - 1][j + 1].bomb === true) { countBombs++; }
       }
       squares[i][j].value = countBombs;
     }
   }
 }
 
-function drawBorders()
-{
+function drawBorders() {
   fill(200);
   stroke(170);
   strokeWeight(5);
@@ -253,7 +224,7 @@ function drawBorders()
   rect(edgeSize + (boxSize * columns) / 6, topBorderSize / 2, columns * 6.66, topBorderSize - topBorderSize / 10, columns);
   rect(edgeSize + (boxSize * columns) / 2, topBorderSize / 2, columns * 6.66, topBorderSize - topBorderSize / 10, columns);
   rect(edgeSize + (boxSize * columns) / 1.2, topBorderSize / 2, columns * 6.66, topBorderSize - topBorderSize / 10, columns);
-  
+
   textAlign(CENTER);
   textSize(columns * 1.5);
   fill(0);
