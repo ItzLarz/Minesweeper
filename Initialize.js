c18b390407aa39a084cf18c12a68f3100fa675bd
ItzLarz <l.y.kruitwagen@gmail.com>
2021-10-29 22:02:12 +0200

    Music, settings, credits

--- Initialize.js
+++ Initialize.js
@@ -2,7 +2,7 @@ document.oncontextmenu = function () {
   return false;
 };
 
-const bombs = 60;
+const bombs = 30;
 const columns = 40;
 const rows = 20;
 const boxSize = 30;
@@ -17,12 +17,16 @@ var uncoverList = [];
 var gameOver = false;
 var win = false;
 var defeat = false;
+var gamePlaying = false;
 var bombCount = bombs;
 var interval = 0;
 var minutes = 0;
 var seconds = 0;
 var spacer = "0";
-var musicState = true;
+var musicState = false;
+var homeScreenState = true;
+var settingsScreenState = false;
+var creditsScreenState = false;
 
 let square0;
 let square1;
@@ -39,6 +43,8 @@ let squareMine;
 let squareNotMine;
 let squareRedMine;
 let home;
+let musicButton;
+let noMusicButton;
 
 function preload() {
   square0 = loadImage("img/0.png");
@@ -57,21 +63,28 @@ function preload() {
   squareRedMine = loadImage("img/redmine.png");
   homeButton = loadImage("img/home.png");
   musicButton = loadImage("img/music.png");
-  noMusicButton = loadImage("img/nomucic.png");
+  noMusicButton = loadImage("img/nomusic.png");
+  stock = loadImage("img/stock.png");
+  play = loadImage("img/play.png");
+  settings = loadImage("img/settings.png");
+  credits = loadImage("img/credits.png");
+  
+  soundFormats("mp3");
+  music = loadSound("sound/music.mp3");
+  music2 = loadSound("sound/music2.mp3");
+  music3 = loadSound("sound/music3.mp3");
+  music4 = loadSound("sound/music4.mp3");
+  music5 = loadSound("sound/music5.mp3");
+  
 }
 
 function setup() {
-  createCanvas(2 * edgeSize + (boxSize * columns), topBorderSize + bottomBorderSize + 2 * edgeSize + (boxSize * rows));
-  background(100);
-  drawField();
-  selectBombs();
-  calcValue();
-  drawBorders();
-  console.log(squares);
+  createCanvas(0, 0);
+  homeScreen();
 }
 
 function draw() {
-  if (!gameOver) {
+  if (!gameOver && !homeScreenState && gamePlaying) {
     if (millis() - interval >= 1000) {
       interval = millis();
       seconds += 1;
@@ -97,6 +110,85 @@ function draw() {
 
       text(minutes + ":" + spacer + seconds, edgeSize + (boxSize * columns) / 1.2, topBorderSize / 1.33);
     }
+  }
+}
+
+function init() {
+  squares = [];
+  bombList = [];
+  uncoverList = [];
+  gameOver = false;
+  win = false;
+  defeat = false;
+  gamePlaying = false;
+  bombCount = bombs;
+  interval = 0;
+  minutes = 0;
+  seconds = -1;
+  spacer = "0";
+
+  resizeCanvas(2 * edgeSize + (boxSize * columns), topBorderSize + bottomBorderSize + 2 * edgeSize + (boxSize * rows));
+  background(100);
+  drawField();
+  selectBombs();
+  calcValue();
+  drawBorders();
+  console.log(squares);
+}
+
+function homeScreen() {
+  resizeCanvas(1600, 850);
+  background(100);
+  image(stock, 0, 0, 1600, 850);
+
+  imageMode(CENTER);
+  image(play, 800, 425, 150, 150);
+
+  imageMode(CORNER);
+  image(settings, 20, 730, 100, 100);
+  image(credits, 1480, 730, 100, 100);
+  
+}
+
+function settingsScreen() {
+  imageMode(CORNER);
+  image(stock, 0, 0, 1600, 850);
+  image(homeButton, 1480, 730, 100, 100);
+}
+
+function creditsScreen() {
+  imageMode(CORNER);
+  image(stock, 0, 0, 1600, 850);
+  image(homeButton, 1480, 730, 100, 100);
+}
+
+function gameOverScreen() {
+  if (defeat) {
+    fill(200);
+    stroke(170);
+    strokeWeight(5);
+    rectMode(CENTER);
+    rect(edgeSize + (boxSize * columns) / 2, topBorderSize / 2, columns * 6.66, topBorderSize - topBorderSize / 10, columns);
+
+    textAlign(CENTER);
+    textSize(columns * 1.3);
+    fill(0);
+    strokeWeight(0);
+    text("Try again?", edgeSize + (boxSize * columns) / 2, topBorderSize / 1.6)
+  }
+
+  if (win) {
+    fill(200);
+    stroke(170);
+    strokeWeight(5);
+    rectMode(CENTER);
+    rect(edgeSize + (boxSize * columns) / 2, topBorderSize / 2, columns * 6.66, topBorderSize - topBorderSize / 10, columns);
+
+    textAlign(CENTER);
+    textSize(columns * 1.3);
+    fill(0);
+    strokeWeight(0);
+    text("You won!", edgeSize + (boxSize * columns) / 2, topBorderSize / 1.6)
   }
 }
 
@@ -136,85 +228,85 @@ function calcValue() {
   for (i = 0; i < columns; i++) {
     for (j = 0; j < rows; j++) {
       let countBombs = 0;
-      if (squares[i][j].bomb === true) {
+      if (squares[i][j].bomb == true) {
         continue;
       }
 
       // Upper left corner
-      if (i === 0 && j === 0) {
-        if (squares[0][1].bomb === true) { countBombs++; }
-        if (squares[1][0].bomb === true) { countBombs++; }
-        if (squares[1][1].bomb === true) { countBombs++; }
+      if (i == 0 && j == 0) {
+        if (squares[0][1].bomb == true) { countBombs++; }
+        if (squares[1][0].bomb == true) { countBombs++; }
+        if (squares[1][1].bomb == true) { countBombs++; }
       }
 
       // Upper right corner
-      else if (i === columns - 1 && j === 0) {
-        if (squares[columns - 2][0].bomb === true) { countBombs++; }
-        if (squares[columns - 1][1].bomb === true) { countBombs++; }
-        if (squares[columns - 2][1].bomb === true) { countBombs++; }
+      else if (i == columns - 1 && j == 0) {
+        if (squares[columns - 2][0].bomb == true) { countBombs++; }
+        if (squares[columns - 1][1].bomb == true) { countBombs++; }
+        if (squares[columns - 2][1].bomb == true) { countBombs++; }
       }
 
       // Bottom left corner
-      else if (i === 0 && j === rows - 1) {
-        if (squares[0][rows - 2].bomb === true) { countBombs++; }
-        if (squares[1][rows - 1].bomb === true) { countBombs++; }
-        if (squares[1][rows - 2].bomb === true) { countBombs++; }
+      else if (i == 0 && j == rows - 1) {
+        if (squares[0][rows - 2].bomb == true) { countBombs++; }
+        if (squares[1][rows - 1].bomb == true) { countBombs++; }
+        if (squares[1][rows - 2].bomb == true) { countBombs++; }
       }
 
       // Bottom right corner
-      else if (i === columns - 1 && j === rows - 1) {
-        if (squares[columns - 1][rows - 2].bomb === true) { countBombs++; }
-        if (squares[columns - 2][rows - 1].bomb === true) { countBombs++; }
-        if (squares[columns - 2][rows - 2].bomb === true) { countBombs++; }
+      else if (i == columns - 1 && j == rows - 1) {
+        if (squares[columns - 1][rows - 2].bomb == true) { countBombs++; }
+        if (squares[columns - 2][rows - 1].bomb == true) { countBombs++; }
+        if (squares[columns - 2][rows - 2].bomb == true) { countBombs++; }
       }
 
       // Left wall
-      else if (i === 0 && j != 0) {
-        if (squares[i][j - 1].bomb === true) { countBombs++; }
-        if (squares[i + 1][j - 1].bomb === true) { countBombs++; }
-        if (squares[i + 1][j].bomb === true) { countBombs++; }
-        if (squares[i + 1][j + 1].bomb === true) { countBombs++; }
-        if (squares[i][j + 1].bomb === true) { countBombs++; }
+      else if (i == 0 && j != 0) {
+        if (squares[i][j - 1].bomb == true) { countBombs++; }
+        if (squares[i + 1][j - 1].bomb == true) { countBombs++; }
+        if (squares[i + 1][j].bomb == true) { countBombs++; }
+        if (squares[i + 1][j + 1].bomb == true) { countBombs++; }
+        if (squares[i][j + 1].bomb == true) { countBombs++; }
       }
 
       // Right wall
-      else if (i === columns - 1 && j != 0) {
-        if (squares[i][j - 1].bomb === true) { countBombs++; }
-        if (squares[i - 1][j - 1].bomb === true) { countBombs++; }
-        if (squares[i - 1][j].bomb === true) { countBombs++; }
-        if (squares[i - 1][j + 1].bomb === true) { countBombs++; }
-        if (squares[i][j + 1].bomb === true) { countBombs++; }
+      else if (i == columns - 1 && j != 0) {
+        if (squares[i][j - 1].bomb == true) { countBombs++; }
+        if (squares[i - 1][j - 1].bomb == true) { countBombs++; }
+        if (squares[i - 1][j].bomb == true) { countBombs++; }
+        if (squares[i - 1][j + 1].bomb == true) { countBombs++; }
+        if (squares[i][j + 1].bomb == true) { countBombs++; }
       }
 
       // Upper wall
-      else if (i != 0 && j === 0) {
-        if (squares[i - 1][j].bomb === true) { countBombs++; }
-        if (squares[i - 1][j + 1].bomb === true) { countBombs++; }
-        if (squares[i][j + 1].bomb === true) { countBombs++; }
-        if (squares[i + 1][j + 1].bomb === true) { countBombs++; }
-        if (squares[i + 1][j].bomb === true) { countBombs++; }
+      else if (i != 0 && j == 0) {
+        if (squares[i - 1][j].bomb == true) { countBombs++; }
+        if (squares[i - 1][j + 1].bomb == true) { countBombs++; }
+        if (squares[i][j + 1].bomb == true) { countBombs++; }
+        if (squares[i + 1][j + 1].bomb == true) { countBombs++; }
+        if (squares[i + 1][j].bomb == true) { countBombs++; }
       }
 
       // Bottom wall
-      else if (i != 0 && j === rows - 1) {
-        if (squares[i - 1][j].bomb === true) { countBombs++; }
-        if (squares[i - 1][j - 1].bomb === true) { countBombs++; }
-        if (squares[i][j - 1].bomb === true) { countBombs++; }
-        if (squares[i + 1][j - 1].bomb === true) { countBombs++; }
-        if (squares[i + 1][j].bomb === true) { countBombs++; }
+      else if (i != 0 && j == rows - 1) {
+        if (squares[i - 1][j].bomb == true) { countBombs++; }
+        if (squares[i - 1][j - 1].bomb == true) { countBombs++; }
+        if (squares[i][j - 1].bomb == true) { countBombs++; }
+        if (squares[i + 1][j - 1].bomb == true) { countBombs++; }
+        if (squares[i + 1][j].bomb == true) { countBombs++; }
       }
 
       // Rest of the squares
       else if (i < columns && j < rows) {
-        if (squares[i - 1][j].bomb === true) { countBombs++; }
-        if (squares[i - 1][j - 1].bomb === true) { countBombs++; }
-        if (squares[i][j - 1].bomb === true) { countBombs++; }
-        if (squares[i + 1][j - 1].bomb === true) { countBombs++; }
+        if (squares[i - 1][j].bomb == true) { countBombs++; }
+        if (squares[i - 1][j - 1].bomb == true) { countBombs++; }
+        if (squares[i][j - 1].bomb == true) { countBombs++; }
+        if (squares[i + 1][j - 1].bomb == true) { countBombs++; }
 
-        if (squares[i + 1][j].bomb === true) { countBombs++; }
-        if (squares[i + 1][j + 1].bomb === true) { countBombs++; }
-        if (squares[i][j + 1].bomb === true) { countBombs++; }
-        if (squares[i - 1][j + 1].bomb === true) { countBombs++; }
+        if (squares[i + 1][j].bomb == true) { countBombs++; }
+        if (squares[i + 1][j + 1].bomb == true) { countBombs++; }
+        if (squares[i][j + 1].bomb == true) { countBombs++; }
+        if (squares[i - 1][j + 1].bomb == true) { countBombs++; }
       }
       squares[i][j].value = countBombs;
     }
@@ -257,4 +349,16 @@ function drawBorders() {
   }
 
   imageMode(CORNER);
+}
+
+function toggleMusic() {
+  if (music.isLoaded()) {
+    if (musicState) {
+      music.loop();
+    }
+
+    else if (!musicState) {
+      music.pause();
+    }
+  }
 }
---
+++ MouseClicked.js
@@ -0,0 +1,183 @@
+function mouseReleased() {
+  let x = mouseX;
+  let y = mouseY;
+  let button;
+  
+  if (mouseButton == LEFT) {
+    button = "left";
+  }
+  
+  else if (mouseButton == RIGHT) {
+    button = "right";
+  }
+
+  if (!homeScreenState) {
+    if (!gameOver) {
+      let col = Math.floor((x - edgeSize) / boxSize);
+      let row = Math.floor((y - edgeSize - topBorderSize) / boxSize);
+      let square;
+
+      for (i = 0; i < columns; i++) {
+        for (j = 0; j < rows; j++) {
+          if (squares[i][j].col == col && squares[i][j].row == row) {
+            square = squares[i][j];
+          }
+        }
+      }
+
+      try {
+        if (button == "left" && square.covered == true && square.marked == false) {
+          gamePlaying = true;
+          square.covered = false;
+
+          if (square.bomb == true) {
+            gameOver = true;
+            defeat = true;
+            image(squareRedMine, square.rawx, square.rawy, boxSize, boxSize);
+            for (i = 0; i < columns; i++) {
+              for (j = 0; j < rows; j++) {
+                if (squares[i][j].bomb == true && squares[i][j].covered == true && squares[i][j].marked == false) {
+                  uncover(squares[i][j]);
+                }
+
+                else if (squares[i][j].marked == true && squares[i][j].bomb == false) {
+                  image(squareNotMine, squares[i][j].rawx, squares[i][j].rawy, boxSize, boxSize);
+                }
+              }
+            }
+
+            gameOverScreen();
+          }
+
+          else if (square.bomb == false) {
+            uncover(square);
+            let gameOverCheck = true;
+            for (i = 0; i < columns; i++) {
+              for (j = 0; j < rows; j++) {
+                if (squares[i][j].covered == true && squares[i][j].bomb == false) {
+                  gameOverCheck = false;
+                  break;
+                }
+              }
+
+              if (!gameOverCheck) {
+                break;
+              }
+            }
+
+            if (gameOverCheck) {
+              gameOver = true;
+              win = true;
+
+              for (i = 0; i < columns; i++) {
+                for (j = 0; j < rows; j++) {
+                  if (squares[i][j].bomb == true && squares[i][j].covered == true && squares[i][j].marked == false) {
+                    squares[i][j].marked = false;
+                    squares[i][j].covered = false;
+                    image(squareFlag, squares[i][j].rawx, squares[i][j].rawy, boxSize, boxSize);
+                    bombCount--
+                  }
+                }
+              }
+
+              textAlign(CENTER);
+              textSize(columns);
+              strokeWeight(0);
+              fill(200);
+              rect(edgeSize + (boxSize * columns) / 6, topBorderSize / 1.55, columns * 6, topBorderSize - topBorderSize / 1.4);
+              fill(0);
+              text(bombCount, edgeSize + (boxSize * columns) / 6, topBorderSize / 1.33);
+
+              gameOverScreen();
+            }
+          }
+        }
+
+        else if (button == "right" && square.covered == true) {
+
+
+          if (square.marked == false) {
+            square.marked = true;
+            image(squareFlag, square.rawx, square.rawy, boxSize, boxSize);
+            bombCount--;
+          }
+
+          else if (square.marked == true) {
+            square.marked = false;
+            image(squareBlank, square.rawx, square.rawy, boxSize, boxSize);
+            bombCount++;
+          }
+
+          textAlign(CENTER);
+          textSize(columns);
+          strokeWeight(0);
+          fill(200);
+          rect(edgeSize + (boxSize * columns) / 6, topBorderSize / 1.55, columns * 6, topBorderSize - topBorderSize / 1.4);
+          fill(0);
+          text(bombCount, edgeSize + (boxSize * columns) / 6, topBorderSize / 1.33);
+        }
+      }
+
+      catch (TypeError) { }
+    }
+
+    if (button == "left" && x > edgeSize + (boxSize * columns / 2) - (columns * 3.33) && x < edgeSize + (boxSize * columns / 2) + (columns * 3.33) && y > topBorderSize / 20 && y < topBorderSize - (topBorderSize / 20)) {
+      init();
+    }
+
+    else if (button == "left" && x > 0.33 * (2 * edgeSize + (boxSize * columns)) - 0.5 * buttonSize && x < 0.33 * (2 * edgeSize + (boxSize * columns)) + 0.5 * buttonSize && y > 0.5 * bottomBorderSize + topBorderSize + 2 * edgeSize + (boxSize * rows) - 0.5 * buttonSize && y < 0.5 * bottomBorderSize + topBorderSize + 2 * edgeSize + (boxSize * rows) + 0.5 * buttonSize) {
+      homeScreenState = true;
+      homeScreen();
+    }
+
+    else if (button == "left" && x > 0.66 * (2 * edgeSize + (boxSize * columns)) - 0.5 * buttonSize && x < 0.66 * (2 * edgeSize + (boxSize * columns)) + 0.5 * buttonSize && y > 0.5 * bottomBorderSize + topBorderSize + 2 * edgeSize + (boxSize * rows) - 0.5 * buttonSize && y < 0.5 * bottomBorderSize + topBorderSize + 2 * edgeSize + (boxSize * rows) + 0.5 * buttonSize) {
+      fill(100);
+      imageMode(CENTER);
+
+      rect(0.66 * (2 * edgeSize + (boxSize * columns)), 0.5 * bottomBorderSize + topBorderSize + 2 * edgeSize + (boxSize * rows), buttonSize, buttonSize);
+      if (musicState) {
+        musicState = false;
+        image(noMusicButton, 0.66 * (2 * edgeSize + (boxSize * columns)), 0.5 * bottomBorderSize + topBorderSize + 2 * edgeSize + (boxSize * rows), buttonSize, buttonSize);
+      }
+
+      else if (!musicState) {
+        musicState = true;
+        image(musicButton, 0.66 * (2 * edgeSize + (boxSize * columns)), 0.5 * bottomBorderSize + topBorderSize + 2 * edgeSize + (boxSize * rows), buttonSize, buttonSize);
+      }
+
+      imageMode(CORNER);
+      toggleMusic();
+    }
+  }
+
+  else if (homeScreenState && !settingsScreenState && !creditsScreenState) {
+    if (button == "left" && x > 725 && x < 875 && y > 350 && y < 500) {
+      homeScreenState = false;
+      init();
+    }
+
+    else if (button == "left" && x > 20 && x < 120 && y > 730 && y < 830) {
+      settingsScreenState = true;
+      settingsScreen();
+    }
+
+    else if (button == "left" && x > 1480 && x < 1580 && y > 730 && y < 830) {
+      creditsScreenState = true;
+      creditsScreen();
+    }
+  }
+
+  else if (homeScreenState && settingsScreenState && !creditsScreenState) {
+    if (button == "left" && x > 1480 && x < 1580 && y > 730 && y < 830) {
+      settingsScreenState = false;
+      homeScreen();
+    }
+  }
+
+  else if (homeScreenState && !settingsScreenState && creditsScreenState) {
+    if (button == "left" && x > 1480 && x < 1580 && y > 730 && y < 830) {
+      creditsScreenState = false;
+      homeScreen();
+    }
+  }
+}
